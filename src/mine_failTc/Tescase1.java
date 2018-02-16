dfc8024f46b39c86299732aae7aeeccfdaccb6e1
hp <hp@hp-PC>
2018-02-16 14:55:06 +0530

    testttfgvb

---
+++ src/mine_failTc/Tescase1.java
@@ -0,0 +1,19 @@
+package mine_failTc;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Tescase1 
+
+{
+
+	@Test
+	public void testcase()
+	{
+		System.out.println("Tc 1 Passed");
+	}
+}
---
+++ src/mine_failTc/Tescase2.java
@@ -0,0 +1,21 @@
+package mine_failTc;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Tescase2 
+
+{
+
+	
+	@Test
+	public void testcase()
+	{
+		
+		System.out.println("Tc 2 Passed");
+	}
+}
