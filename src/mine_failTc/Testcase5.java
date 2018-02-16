51d39bd8e0b38de739fc97203d62155d7be9ae11
hp <hp@hp-PC>
2018-02-16 19:28:00 +0530

    test

---
+++ src/mine_failTc/Testcase5.java
@@ -0,0 +1,14 @@
+package mine_failTc;
+
+import org.testng.annotations.Test;
+
+public class Testcase5 {
+
+	@Test
+	public void testcase()
+	{
+		//AssertJUnit.assertTrue(false);
+		System.out.println("============Tc 5 ============");
+	}
+
+}
