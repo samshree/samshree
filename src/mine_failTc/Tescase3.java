2db035ca9fb548a7caa0790e2915cc89849045eb
hp <hp@hp-PC>
2018-02-16 14:00:29 +0530

    first update

---
+++ src/mine_failTc/Tescase3.java
@@ -0,0 +1,22 @@
+package mine_failTc;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Tescase3 
+
+{
+
+	
+	@Test
+	public void testcase()
+	{
+		
+		System.out.println("Tc 3 Passed");
+	}
+}
